dd0e6ffcebdad60e3abc80c34447c5bbf4c39b00
btroncone <btroncone@gmail.com>
2013-09-16 12:16:13 -0400

    Array comparison solved in javascript

---
+++ ArrayComparison/src/arrayCompare/arrayComparison.js
@@ -0,0 +1,22 @@
+﻿var arrayOne = [1, 3, 5, 8];
+var arrayTwo = [1, 4, 5, 6];
+
+
+var removeDuplicates = function(arrOne, arrTwo) {
+    var arrayHolder = [];
+
+    for (var i = 0; i < arrOne.length; i++) {
+        for (var j = 0; j < arrTwo.length; j++) {
+            if (arrOne[i] === arrTwo[j]) {
+                arrOne.splice(i, 1);
+                arrTwo.splice(j, 1);
+            }
+        }
+    }
+    arrayHolder.push(arrOne);
+    arrayHolder.push(arrTwo);
+    return arrayHolder;
+};
+
+var duplicatesRemoved = removeDuplicates(arrayOne, arrayTwo);
+console.log(duplicatesRemoved);
